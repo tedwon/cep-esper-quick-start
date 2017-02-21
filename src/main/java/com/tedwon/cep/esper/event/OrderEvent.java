6515ba391d39ffe1578d5cb600fafdb1f6402c54
Ted Won <iamtedwon@gmail.com>
2017-02-22 00:32:20 +0900

    Update esper version to 5.5

--- src/main/java/com/tedwon/cep/esper/event/OrderEvent.java
+++ src/main/java/com/tedwon/cep/esper/event/OrderEvent.java
@@ -2,36 +2,33 @@ package com.tedwon.cep.esper.event;
 
 /**
  * Order Event Class.
- * <p/>See http://esper.codehaus.org/tutorials/tutorial/quickstart.html
- *
- * <p/>Creating a Java Event Class
- *
- * @author <a href=mailto:tedwon@kthcorp.com">Ted Won</a>
- * @version 1.0
+ * <p/>See http://www.espertech.com/esper/quickstart.php
+ * <p>
+ * <p/>Creating a Java POJO Event Class
  */
 public class OrderEvent {
 
-    private String itemName;
-    private double price;
+   private String itemName;
+   private double price;
 
-    public OrderEvent(String itemName, double price) {
-        this.itemName = itemName;
-        this.price = price;
-    }
+   public OrderEvent(String itemName, double price) {
+      this.itemName = itemName;
+      this.price = price;
+   }
 
-    public String getItemName() {
-        return itemName;
-    }
+   public String getItemName() {
+      return itemName;
+   }
 
-    public double getPrice() {
-        return price;
-    }
+   public double getPrice() {
+      return price;
+   }
 
-    @Override
-    public String toString() {
-        return "OrderEvent{" +
-                "itemName='" + itemName + '\'' +
-                ", price=" + price +
-                '}';
-    }
+   @Override
+   public String toString() {
+      return "OrderEvent{" +
+            "itemName='" + itemName + '\'' +
+            ", price=" + price +
+            '}';
+   }
 }
